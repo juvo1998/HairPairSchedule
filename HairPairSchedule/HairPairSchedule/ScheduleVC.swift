78f31b1dd187ed8b0635f17879b00d0b812cf274
Justin Vo <justinvo1998@gmail.com>
2019-06-21 14:32:48 -0400

    First commit

---
+++ HairPairSchedule/HairPairSchedule/ScheduleVC.swift
@@ -0,0 +1,10 @@
+//
+//  ScheduleVC.swift
+//  HairPairSchedule
+//
+//  Created by Justin Vo on 6/20/19.
+//  Copyright © 2019 Justin Vo. All rights reserved.
+//
+
+import Foundation
+import UIKit
